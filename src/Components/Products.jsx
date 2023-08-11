251c372bfc7d61b0c37a9051d6abf8ab9d5ebcd6
Senan747 <111648854+Senan747@users.noreply.github.com>
2023-08-11 09:07:47 +0400

    added product style

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -21,16 +21,15 @@ function Products() {
   const { type } = useParams();
   const { tag } = useParams();
 
-
   useEffect(() => {
     let url;
     if (location.pathname.includes(category)) {
       url = `http://makeup-api.herokuapp.com/api/v1/products.json?brand=&product_type=${type}&product_category=${category}`;
-    }else if (location.pathname.includes(tag)) {
+    } else if (location.pathname.includes(tag)) {
       url = `http://makeup-api.herokuapp.com/api/v1/products.json?brand=&product_type=${type}&product_tags=${tag}`;
     } else if (location.pathname.includes(type)) {
       url = `http://makeup-api.herokuapp.com/api/v1/products.json?brand=&product_type=${type}`;
-    }  else {
+    } else {
       url =
         "http://makeup-api.herokuapp.com/api/v1/products.json?rating_less_than=5";
     }
@@ -45,10 +44,9 @@ function Products() {
       });
   }, [location.pathname, type, category]);
 
-
   const handleRating = (rating) => {
-    setRating(rating)
-  }
+    setRating(rating);
+  };
 
   const handleProductClick = (product) => {
     setProductData((prevData) => [...prevData, product]);
@@ -75,73 +73,73 @@ function Products() {
   };
 
   return (
-    <div className="mx-auto p-4 flex flex-col mt-[100px]">
+    <div className="mx-auto p-4 flex flex-col items-end mt-[100px]">
       <ul className="flex flex-wrap flex-row justify-around items-start">
         {products.map((product) =>
           product.image_link ? (
             <Link to="/product">
-            <li
-              key={product.id}
-              className="border p-4 mb-10 rounded-lg shadow-md max-w-[300px] cursor-pointer"
-              onClick={() => setProduct(product)}
-            >
-              <img
-                src={product.image_link}
-                alt={product.name}
-                className="w-40 h-40 object-cover mx-auto mb-4"
-              />
-              <h3 className="text-xl font-semibold mb-2">{product.name}</h3>
-              <p className="text-gray-600 mb-2">
-                {product.description
-                  ? product.description.slice(0, 70) + "..."
-                  : product.description}
-              </p>
-              <div className="flex-end">
-                <ul className="flex space-x-1">
-                  {handleRating(product.rating)}
-                  <Stars />
-                </ul>
-                <div className="text-lg font-semibold flex flex-row">
-                  <p className="mr-1">{product.price}</p>
-                  <p>
-                    {product.price_sign === null ? "$" : product.price_sign}
-                  </p>
-                </div>
-
-                <div className="flex flex-row items-end justify-between">
-                  <ul className="flex flex-wrap space-x-2">
-                    {product.product_colors.map((color) => (
-                      <li
-                        className="w-6 h-6 rounded-full"
-                        style={{ backgroundColor: color.hex_value }}
-                        title={color.colour_name}
-                      />
-                    ))}
+              <li
+                key={product.id}
+                className="border p-4 mb-10 rounded-lg shadow-md max-w-[300px] min-h-[500px] cursor-pointer"
+                onClick={() => setProduct(product)}
+              >
+                <img
+                  src={product.image_link}
+                  alt={product.name}
+                  className="w-40 h-40 object-cover mx-auto mb-4"
+                />
+                <h3 className="text-xl font-semibold mb-2">{product.name}</h3>
+                <p className="text-gray-600 mb-2">
+                  {product.description
+                    ? product.description.slice(0, 70) + "..."
+                    : product.description}
+                </p>
+                <div className="flex-end">
+                  <ul className="flex space-x-1">
+                    {handleRating(product.rating)}
+                    <Stars />
                   </ul>
-                  {userData !== null ? (
-                    <div
-                      className="rounded-[100%] hover:bg-gega-light-grey duration-30"
-                      onClick={() => {
-                        handleProductClick(product);
-                        handleNotficition();
-                      }}
-                    >
-                      <SlBasket className="mr-[10px] text-5xl py-2" />
-                    </div>
-                  ) : (
-                    <div
-                      className="rounded-[100%] hover:bg-gega-light-grey duration-300"
-                      onClick={(event) => {
-                        setShow(true);
-                        event.stopPropagation();
-                      }}
-                    >
-                      <SlBasket className="mr-[10px] text-5xl py-2" />
-                    </div>
-                  )}
+                  <div className="text-lg font-semibold flex flex-row">
+                    <p className="mr-1">{product.price}</p>
+                    <p>
+                      {product.price_sign === null ? "$" : product.price_sign}
+                    </p>
+                  </div>
+
+                  <div className="flex flex-row items-end justify-between">
+                    <ul className="flex flex-wrap space-x-2">
+                      {product.product_colors.map((color) => (
+                        <li
+                          className="w-6 h-6 rounded-full"
+                          style={{ backgroundColor: color.hex_value }}
+                          title={color.colour_name}
+                        />
+                      ))}
+                    </ul>
+                    {userData !== null ? (
+                      <div
+                        className="rounded-[100%] hover:bg-gega-light-grey duration-30"
+                        onClick={() => {
+                          handleProductClick(product);
+                          handleNotficition();
+                        }}
+                      >
+                        <SlBasket className="mr-[10px] text-5xl py-2" />
+                      </div>
+                    ) : (
+                      <div
+                        className="rounded-[100%] hover:bg-gega-light-grey duration-300"
+                        onClick={(event) => {
+                          setShow(true);
+                          event.stopPropagation();
+                        }}
+                      >
+                        <SlBasket className="mr-[10px] text-5xl py-2" />
+                      </div>
+                    )}
+                  </div>
                 </div>
-              </div>
-            </li>
+              </li>
             </Link>
           ) : null
         )}
@@ -169,10 +167,18 @@ function Products() {
         )}
       </div>
       <div className="w-[100px] cursor-pointer mr-[50px] font-semibold">
-        <Link to="/rating" className="flex items-center justify-around">
-          <p>For more</p>
-          <AiOutlineArrowRight />
-        </Link>
+        {location.pathname.includes("rating") ||
+        location.pathname.includes("product") ||
+        location.pathname.includes("result") ? (
+          <div>
+            
+          </div>
+        ) : (
+          <Link to="/rating" className="flex items-center justify-around ml">
+            <p>For more</p>
+            <AiOutlineArrowRight />
+          </Link>
+        )}
       </div>
     </div>
   );
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,45 +1,61 @@
 import React from "react";
 import { useUserData } from "../UserDataContext";
+import Navbar from "../Components/Navbar";
+import Menu from "../Components/Menu";
 import Stars from "../Components/Stars";
 
 function Product() {
   const { product } = useUserData();
 
   return (
-    <div className="border p-4 mb-10">
-      <img
-        src={product.image_link}
-        alt={product.name}
-        className="w-40 h-40 object-cover mx-auto mb-4"
-      />
-      <h3 className="text-xl font-semibold mb-2">{product.name}</h3>
-      <p className="text-gray-600 mb-4">{product.description}</p>
-      <div className="flex justify-between items-center">
-        <div className="text-lg font-semibold">
-          <p className="flex items-center">
-            <span className="mr-1">{product.price}</span>
-            <span>
-              {product.price_sign === null ? "$" : product.price_sign}
-            </span>
-            <ul>
-              <Stars />
-            </ul>
-          </p>
+    <div>
+      <Navbar />
+      <Menu />
+      <div className="flex flex-col gap-10 mt-10">
+        <div className="flex flex-row items-center justify-center">
+          <div className="w-[300px] h-[300px]">
+            <img src={product.image_link} alt="" className="w-full " />
+          </div>
+          <div className="ml-10">
+            <p className="text-2xl font-semibold">{product.name}</p>
+            <p className="mt-5 text-3xl">
+              {product.price}{" "}
+              <span>
+                {product.price_sign === null ? "$" : product.price_sign}
+              </span>
+            </p>
+            <div className="flex space-x-2 pt-5">
+              {product.product_colors.map((color, index) => (
+                <div
+                  key={index}
+                  className="w-6 h-6 rounded-full"
+                  style={{ backgroundColor: color.hex_value }}
+                  title={color.colour_name}
+                />
+              ))}
+            </div>
+            <a href={product.product_link} className="">
+              <button className="mt-10 bg-gega-pink text-gega-white px-16 py-2 rounded-lg">
+                Buy
+              </button>
+            </a>
+          </div>
         </div>
-        <div className="flex items-center space-x-2">
-          {product.product_colors.map((color, index) => (
-            <div
-              key={index}
-              className="w-6 h-6 rounded-full"
-              style={{ backgroundColor: color.hex_value }}
-              title={color.colour_name}
-            />
-          ))}
+        <div className="p-10 text-justify">
+          <h1 className="text-center text-lg font-semibold underline">
+            Description
+          </h1>
+          <p>{product.description}</p>
+          <ul className="pl-8 pt-5">
+            {product.tag_list.map((tag) => (
+              <li className="list-disc">{tag}</li>
+            ))}
+          </ul>
+          <div className="flex flex-row mt-5">
+            <Stars />
+          </div>
         </div>
-        <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none">
-          Buy
-        </button>
-      </div>
+      </div>{" "}
     </div>
   );
 }
