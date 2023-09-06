b2c4318e2a68d8432709e5c1930bbb6bb1d014c5
Senan747 <111648854+Senan747@users.noreply.github.com>
2023-09-06 14:28:58 +0400

    json

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,75 +1,78 @@
-import React, { useState } from "react";
-import { useUserData } from "../UserDataContext";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 
-function Login() {
+import { useNavigate } from "react-router-dom";
+
+
+function LoginPanel(props) {
+  const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const { setUserData } = useUserData();
-  
-  let navigate = useNavigate();
+  const [loginStatus, setLoginStatus] = useState(null);
+  const [user, setUser] = useState([]);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const userData = { username, password };
-    setUserData(userData);
-    navigate("/");
+  const getData = () => {
+    fetch("http://localhost:3004/users")
+      .then((response) => response.json())
+      .then((result) => setUser(result))
+      .catch((error) => console.log("error", error));
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const handleLogin = () => {
+    const evet = user.find(
+      (user) => user.username === username && user.password === password
+    );
+    if (evet) {
+      navigate("/");
+      setLoginStatus("success");
+    } else {
+      setLoginStatus("failure");
+    }
+  };
+
+  const handleReset = () => {
+    setUsername("");
+    setPassword("");
+    setLoginStatus(null);
   };
 
   return (
-    <div className="flex flex-row h-screen">
-      <div className="w-[700px] bg-cover">
-        <img src="danla.jpg" alt="" className="h-full w-full" />
-      </div>
-      <div className="flex flex-col justify-start">
-        <div className="w-[150px] h-[100px]">
-          <img src="logo.png" alt="" className="w-full h-full" />
-        </div>
-        <div className="w-[400px] h-[200px] relative top-0">
-          <h1 className="text-5xl text-gega-pink font-Fira font-bold absolute top-[80px] left-[70px] ">
-            Login
-          </h1>
-        </div>
+    <div>
+      {loginStatus === "success" ? (
+        <div>Login oldu</div>
+      ) : loginStatus === "failure" ? (
         <div>
-          <form
-            action=""
-            className="flex flex-col gap-3 items-start ml-[70px]"
-            onSubmit={handleSubmit}
-          >
-            <label htmlFor="Username">Username</label>
+          Login olmadı
+          <button onClick={handleReset}>Return Login</button>
+        </div>
+      ) : (
+        <div>
+          <form className="login-panel-container" noValidate autoComplete="off">
             <input
-              type="Username"
-              name=""
-              id=""
-              className="border-b-2 outline-none"
-              placeholder="user"
+              required
+              label="Username"
+              value={username}
               onChange={(e) => setUsername(e.target.value)}
-              required
             />
-            <label htmlFor="password">Password</label>
             <input
-              type="password"
-              name=""
-              id=""
-              className="border-b-2 w-[230px] outline-none"
-              placeholder="Enter 8 or more characters"
-              onChange={(e) => setPassword(e.target.value)}
               required
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
-            <button
-              type="submit"
-              className=" py-2 px-5 mt-4 rounded-md text-gega-white bg-gega-rose"
-            >
+            <button onClick={handleLogin} variant="contained" color="primary">
               Login
             </button>
-            <Link to="/register">
-              <p className="underline text-gega-rose">Don't have an account?</p>
-            </Link>
           </form>
         </div>
-      </div>
+      )}
     </div>
   );
 }
 
-export default Login;
+export default LoginPanel;
